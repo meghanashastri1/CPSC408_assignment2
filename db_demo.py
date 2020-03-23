ec70de22e6b474f21ad00535efa9868ac5d4660d
meghanashastri1 <meghana360@gmail.com>
2020-03-22 23:47:48 -0700

    added more comments

--- db_demo.py
+++ db_demo.py
@@ -32,74 +32,127 @@ def printMenu():
 #function to print the table
 def optionA():
     print("Displaying all students: ")
+    #SQLite query to select all attributes from all students
     c.execute("SELECT * FROM Students")
+
+    #commiting changes to the table
     conn.commit()
+
+    #store data from table as tuples in variable all_rows
     all_rows = c.fetchall()
+    #for each tuple in list, print tuple
+    #each tuple has the attributes for each student
     for row in all_rows:
         print(row)
 
 
 #function to create a new student
 def optionB():
+    #ask user for first name, last name, gpa, major, and facuty advisor of new student
     First = input("Enter first name of Student: ")
-    Last = input("Enter second name of Student: ")
+    Last = input("Enter last name of Student: ")
     gpa = float(input("Enter the GPA of the student"))
     major = input("Enter major of Student: ")
     advisor = input("Enter the Student's of advisor ")
     deleted = int(input("Enter '1' if student is deleted or '0' if student is not deleted"))
     print("creating student...")
+
+    #executing SQLite query of inserting new student with the attributes the user entered
     c.execute("INSERT INTO Students('FirstName', 'LastName', 'GPA', 'Major', 'FacultyAdvisor', 'isDeleted')"
               "VALUES (?,?,?,?,?,?)", (First, Last, gpa, major, advisor, deleted))
+
+    #commit changes
     conn.commit()
 
 #function to update a student's  major and advisor
 def optionC():
+    #ask user for the Student ID of the student they want to update
     stud = int(input("Enter the ID of the student whose information you want to update."))
     print("you can update the student's major and faculty advisor.")
+
+    #user enters new major and new faculty advisor
     maj = input("Enter the student's new major: ")
     faculty = input("Enter the student's new faculty advisor: ")
+
+    #query updates major and faculty advisor of the studentId which the user entered
+    #update keyword makes sure that not every student record in table is updated
     c.execute("UPDATE Students SET Major = ? WHERE StudentId = ?", (maj, stud,))
     c.execute("UPDATE Students SET FacultyAdvisor = ? WHERE StudentId = ?", (faculty, stud,))
+
+    # commit changes
     conn.commit()
 
 #function to delete a student from table, soft delete
 def optionD():
+    # ask user for the Student ID of the student they want to delete
     studToDelete = int(input("Enter the ID of the student who you want to delete from the table"))
+
+    #query will change isDeleted status to 1, which is considered to be true for if the student is deleted
+    #this is a soft delete, the student record is still in the table
     c.execute("UPDATE Students SET isDeleted = ? WHERE StudentId = ?", (1, studToDelete,))
+
+    #to show that student is deleted, execute query where isDeleted is 0, which will return students that are not deleted
     c.execute("SELECT * FROM Students WHERE isDeleted = 0")
+
+    #commit changes
     conn.commit()
+
+    # store data from table as tuples in variable data
     data = c.fetchall()
-    print(data)
+    # for each tuple in list, print tuple
     for row in data:
         print(row)
 
 #function to search students by either major, gpa, or faculty advisor
 def optionE():
+    #ask user what they want to enter to search for a student
     queryChoice = input("would you like to search for students depending on 'major', 'gpa', or 'advisor'?")
     if queryChoice == "major":
         majorQuery = input("Enter the major you want to search ")
+
+        #selects all student records that have the major which the user entered
         c.execute("SELECT * FROM Students WHERE Major = ?", (majorQuery,))
+
+        #commit changes
         conn.commit()
+
+        # store data from table as tuples in variable data
         data = c.fetchall()
-        print(data)
+        # for each tuple in list, print tuple
         for row in data:
             print(row)
     elif queryChoice == "gpa":
+        #cast gpa as a float so that the GPA in the sqlite table will be a numeric
         qpaQuery = float(input("Enter the gpa you want to search "))
+
+        # selects all student records that have the GPA which the user entered
         c.execute("SELECT * FROM Students WHERE GPA = ?", (qpaQuery,))
+
+        #commit changes
         conn.commit()
+
+        # store data from table as tuples in variable data
         data = c.fetchall()
-        print(data)
+        # for each tuple in list, print tuple
         for row in data:
             print(row)
     elif queryChoice == "advisor":
         advisorQuery = input("Enter the advisor you want to search ")
+
+        # selects all student records that have the faculty advisor which the user entered
         c.execute("SELECT * FROM Students WHERE FacultyAdvisor = ?", (advisorQuery,))
+
+        #commit changes
         conn.commit()
+
+        # store data from table as tuples in variable data
         data = c.fetchall()
-        print(data)
+        # for each tuple in list, print tuple
         for row in data:
             print(row)
+
+
+
 
 #starting program
 printMenu()
